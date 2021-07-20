c3b8c84748e9938d94487c6ddb0d0b94381cefef
yuewei.yang <yuewei.yang@outlook.com>
2021-07-21 07:44:07 +0800

    Update ECR_Auth.sh

--- mirror/ECR_Auth.sh
+++ mirror/ECR_Auth.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 env
 
-aws configure --profile=GlobalECR set aws_access_key_id $ecr_ak
-aws configure --profile=GlobalECR set aws_secret_access_key $ecr_sk
+#aws configure --profile=GlobalECR set aws_access_key_id $ecr_ak
+#aws configure --profile=GlobalECR set aws_secret_access_key $ecr_sk
+
+aws configure --profile=GlobalECR set aws_access_key_id AKIA4QAUTQR6UD2P7GPF
+aws configure --profile=GlobalECR set aws_secret_access_key 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
 aws configure --profile=GlobalECR set default.region us-west-1
